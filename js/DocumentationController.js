6c3389e804b5ec25b1330f9d2d16fe9f3e32e35d
igormalzew <igor_malzew@mail.ru>
2017-08-22 08:07:31 +0300

    Change upload documents

--- js/DocumentationController.js
+++ js/DocumentationController.js
@@ -38,7 +38,8 @@
                         $scope.Ctrls.DisplayDocs.docs.getDocFile(data.id);
                     else {
                         this.value = '';
-                        $scope.Ctrls.DisplayDocs.docs.openTreeItem(data.id);
+                        $scope.Ctrls.DisplayDocs.docs.filterDocsWithTimeOut();
+                        $scope.Ctrls.DisplayDocs.docs.openTreeItem(data.$$hashKey);
                     }
                 }
             },
@@ -75,14 +76,11 @@
                     //    location.reload();
                     //});
                 },
-
                 getDocFile: function (fileId) {
-                   
 
                 },
-
                 filterDocs: function () {
-
+                    
                     let result = [];
                     let searchStr = $scope.Ctrls.DisplayDocs.search.value;
 
@@ -95,7 +93,7 @@
                 },
                 filterItemChildren: function (item, searchStr) {
                     if (item.children == undefined || item.children.length == 0) {
-                        return item.name.toLowerCase().indexOf(searchStr.toLowerCase()) !== -1 ? [{ id: item.id, name: item.name, isFile: item.isFile }] : [];
+                        return item.name.toLowerCase().indexOf(searchStr.toLowerCase()) !== -1 ? [{ $$hashKey: item.$$hashKey, id: item.id, name: item.name, isFile: item.isFile }] : [];
                     }
                     else {
                         let childrenResult = [];
@@ -109,7 +107,7 @@
                         }
 
                         if (item.name.toLowerCase().indexOf(searchStr.toLowerCase()) !== -1)
-                            childrenResult = [{ id: item.id, name: item.name, isFile: item.isFile }].concat(childrenResult);
+                            childrenResult = [{ $$hashKey: item.$$hashKey, id: item.id, name: item.name, isFile: item.isFile }].concat(childrenResult);
 
                         return childrenResult;
                     }
@@ -120,27 +118,49 @@
                     else if ($scope.Ctrls.DisplayDocs.search.value.length == 0)
                         $scope.Ctrls.DisplayDocs.search.results = [];
                 },
-                openTreeItem: function (itemId) {
+                openTreeItem: function (openNodeHashKey) {
                     for (var i = 0; i < this.items.length; i++) {
-                        let isChildOpen = $scope.Ctrls.DisplayDocs.docs.openTreeItemChildren(this.items[i], itemId);
+                        let isChildOpen = $scope.Ctrls.DisplayDocs.docs.openTreeItemChildren(this.items[i], openNodeHashKey);
 
                         this.items[i].isOpen = isChildOpen;
                     }
                 },
-                openTreeItemChildren: function (item, openItemId) {
-                    item.isOpen = item.id === openItemId;
+                openTreeItemChildren: function (item, openNodeHashKey) {
+                    item.isOpen = item.$$hashKey === openNodeHashKey;
 
                     if (item.children == undefined || item.children.length == 0)
                         return item.isOpen && !item.isFile;
 
                     let isChildOpen = false;
-                    for (var i = 0; i < item.children.length; i++) {
-                        isChildOpen = $scope.Ctrls.DisplayDocs.docs.openTreeItemChildren(item.children[i], openItemId) || isChildOpen;
+                    for (let i = 0; i < item.children.length; i++) {
+                        isChildOpen = $scope.Ctrls.DisplayDocs.docs.openTreeItemChildren(item.children[i], openNodeHashKey) || isChildOpen;
                     }
 
                     item.isOpen = item.isOpen || isChildOpen;
                     return item.isOpen;
                 },
+                removeNode: function (deletedNodeHashKey) {
+                    for (let i = 0; i < this.items.length; i++) {
+                        if(this.items[i].$$hashKey === deletedNodeHashKey){
+                            this.items.splice(i, 1);
+                            return;
+                        }
+
+                        $scope.Ctrls.DisplayDocs.docs.removeChildNode(this.items[i], deletedNodeHashKey);
+                    }
+                },
+                removeChildNode: function (node, deletedNodeHashKey) {
+                    if (node.children === null || node.children === undefined || node.children.length === 0) return;
+
+                    for (let i = 0; i < node.children.length; i++) {
+                        if (node.children[i].$$hashKey === deletedNodeHashKey) {
+                            node.children.splice(i, 1);
+                            return;
+                        }
+
+                        $scope.Ctrls.DisplayDocs.docs.removeChildNode(node.children[i], deletedNodeHashKey);
+                    }
+                }
             },
 
             nodeClick: function (data) {
@@ -153,14 +173,14 @@
             admin: {
                 activeAction: null,
                 activeNode: null,
-                setActive: function (action, nodeIndex) {
-                    if (this.activeAction === action && this.activeNode === nodeIndex) {
+                setActive: function (action, node) {
+                    if (this.activeAction === action && this.activeNode === node) {
                         this.activeAction = null;
                         this.activeNode = null;
                     }
                     else {
                         this.activeAction = action;
-                        this.activeNode = nodeIndex;
+                        this.activeNode = node;
                     }
                 },
                 clearActive: function () {
@@ -176,30 +196,227 @@
                         },
                         items: [
                             { id: 1, name: 'Подкатегорию' },
-                            { id: 2, name: 'Документ' }
+                            { id: 2, name: 'Документ' },
+                            { id: 3, name: 'Категорию', isCategory: true }
                         ]
                     },
-                    folderName: '',
                     accessRoles: {
                         items: [],
                         init: function (data) {
                             this.items = data;
+                        },
+                        getItems: function () {
+                            let res = [];
+
+                            this.items.forEach((item) => {
+                                res.push({
+                                    tagId: item.TagId,
+                                    tagName: item.TagName,
+                                    isSelected: true
+                                });
+                            });
+
+                            return res;
                         }
+                    },
+                    getNewFolder: function () {
+                        return {
+                            name: '',
+                            accessRoles: this.accessRoles.getItems(),
+                            getActiveRoles: function () {
+                                let res = [21];
+                                for (var i = 0; i < this.accessRoles.length; i++) {
+                                    if (this.accessRoles[i].isSelected)
+                                        res.push(this.accessRoles[i].tagId);
+                                }
+
+                                if ($scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 3)
+                                    res.push($scope.Ctrls.DisplayDocs.sideBarFilter.activeProduct.TagId);
+
+                                return res;
+                            }
+                        };
+                    },
+                    newFolder: {},
+                    files: {
+                        items: [],
+                        add: function (id, name, value, extension) {
+                            this.items.push({
+                                id: id,
+                                name: name,
+                                extension: extension,
+                                value: value
+                            });
+                        },
+                        remove: function (item) {
+                            for (var i = this.items.length - 1; i >= 0; i--) {
+                                if (this.items[i] === item) {
+                                    this.items.splice(i, 1);
+                                }
+                            }
+                        }
+                    },
+                    isOkDisabled: function () {
+                        if ($scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 1 ||
+                            $scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 3)
+                            return $scope.Ctrls.DisplayDocs.admin.addNode.newFolder.name == '';
+
+                        if ($scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 2)
+                            return $scope.Ctrls.DisplayDocs.admin.addNode.files.items.length == 0;
+                    },
+                    posibleOpenTop: function () {
+                        return $(window).height() > 500 && $('#documentTree').height() > 320;
+                    },
+                    posibleOpenTopNodeTree: function (id, elementHeight) {
+                        return document.getElementById(id) != null && document.getElementById(id).getBoundingClientRect().top > elementHeight;
+                    },
+                    start: function (node) {
+                        $scope.Ctrls.DisplayDocs.admin.setActive(this.id, node);
+
+                        $scope.Ctrls.DisplayDocs.admin.addNode.files.items = [];
+                        this.newFolder = $scope.Ctrls.DisplayDocs.admin.addNode.getNewFolder();
+
+                        $scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId = node === 'isAddCategory' ? 3 : 1;
+                        
+                    },
+                    getSaveData: function () {
+                        if ($scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 1 ||
+                            $scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 3)
+                            return {
+                                Id: null,
+                                ParentId: $scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 1 ? $scope.Ctrls.DisplayDocs.admin.activeNode.id : null,
+                                Topic: $scope.Ctrls.DisplayDocs.admin.addNode.newFolder.name,
+                                Tags: $scope.Ctrls.DisplayDocs.admin.addNode.newFolder.getActiveRoles(),
+                                Files: [],
+                                StayOldFiles: true,
+                                IsCategory: $scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 3
+                            }
+                        else
+                            return {
+                                Id: $scope.Ctrls.DisplayDocs.admin.activeNode.id,
+                                ParentId: null,
+                                Topic: $scope.Ctrls.DisplayDocs.admin.activeNode.name,
+                                Tags:  $scope.Ctrls.DisplayDocs.admin.activeNode.accessRoles,
+                                Files: $scope.Ctrls.DisplayDocs.admin.addNode.files.items,
+                                StayOldFiles: true
+                            }
+                    },
+                    save: function () {
+                        if (this.isOkDisabled()) return;
+
+                        //$scope.Ctrls.DisplayDocs.search.isLoading = true;
+
+                        //$http({
+                        //    url: '',
+                        //    method: "POST",
+                        //    data: {
+                        //        data: this.getSaveData()
+                        //    }
+                        //}).then(function (res) {
+                        //    if (res.data.status === 'success') {
+
+                        //        if ($scope.Ctrls.DisplayDocs.admin.addNode.typeOfNode.activeTypeId === 3) 
+                        //            $scope.Ctrls.DisplayDocs.docs.items.push(res.data.data[0]);
+                        //        else {
+                        //            $scope.Ctrls.DisplayDocs.admin.activeNode.children = res.data.data;
+                        //            $scope.Ctrls.DisplayDocs.admin.activeNode.isOpen = true;
+                        //        }
+
+                        //        $scope.Ctrls.DisplayDocs.admin.clearActive()
+                        //    }
+                        //    else
+                        //        alert('Произошла ошибка. Обратитесь в службу поддержки.');
+
+                        //    $scope.Ctrls.DisplayDocs.search.isLoading = false;
+
+                        //}, function () {
+                        //    location.reload();
+                        //});
                     }
                 },
                 editNode: {
-                    id: 2
+                    id: 2,
+                    editName: '',
+                    isOkDisabled: function () {
+                        return this.editName == '' || $scope.Ctrls.DisplayDocs.admin.activeNode == null || this.editName == $scope.Ctrls.DisplayDocs.admin.activeNode.name;
+                    },
+                    start: function (node) {
+                        $scope.Ctrls.DisplayDocs.admin.setActive(this.id, node);
+
+                        this.editName = $scope.Ctrls.DisplayDocs.admin.activeNode.name;
+                    },
+                    getSaveData: function () {
+                        return {
+                            Id: $scope.Ctrls.DisplayDocs.admin.activeNode.id,
+                            ParentId: null,
+                            Topic: this.editName,
+                            Tags: $scope.Ctrls.DisplayDocs.admin.activeNode.accessRoles,
+                            Files: [],
+                            StayOldFiles: true
+                        }
+                    },
+                    save: function () {
+                        if (this.isOkDisabled()) return;
+
+                        //$scope.Ctrls.DisplayDocs.search.isLoading = true;
+
+                        //$http({
+                        //    url: '',
+                        //    method: "POST",
+                        //    data: {
+                        //        data: this.getSaveData()
+                        //    }
+                        //}).then(function (res) {
+                        //    if (res.data.status === 'success') {
+
+                        //        $scope.Ctrls.DisplayDocs.admin.activeNode.name = $scope.Ctrls.DisplayDocs.admin.editNode.editName;
+                        //        $scope.Ctrls.DisplayDocs.admin.clearActive()
+                        //    }
+                        //    else
+                        //        alert('Произошла ошибка. Обратитесь в службу поддержки.');
+
+                        //    $scope.Ctrls.DisplayDocs.search.isLoading = false;
+
+                        //}, function () {
+                        //    location.reload();
+                        //});
+                    }
                 },
                 deleteNode: {
-                    id: 3
+                    id: 3,
+                    start: function (node) {
+                        $scope.Ctrls.DisplayDocs.admin.setActive(this.id, node);
+                    },
+                    save: function () {
+                        //$scope.Ctrls.DisplayDocs.search.isLoading = true;
+
+                        //$http({
+                        //    url: '',
+                        //    method: "POST",
+                        //    data: {
+                        //        nodeId: $scope.Ctrls.DisplayDocs.admin.activeNode.id,
+                        //        isFile: $scope.Ctrls.DisplayDocs.admin.activeNode.isFile
+                        //    }
+                        //}).then(function (res) {
+                        //    if (res.data.status === 'success') {
+                        //        $scope.Ctrls.DisplayDocs.docs.removeNode($scope.Ctrls.DisplayDocs.admin.activeNode.$$hashKey);
+                        //        $scope.Ctrls.DisplayDocs.admin.clearActive();
+                        //    }
+                        //    else
+                        //        alert('Произошла ошибка. Обратитесь в службу поддержки.');
+
+                        //    $scope.Ctrls.DisplayDocs.search.isLoading = false;
+
+                        //}, function () {
+                        //    location.reload();
+                        //});
+                    }
                 }
             }
         }
 
     };
-
-
-
+                
 
     $scope.FakeServer = {
         getCategories: function () {
@@ -267,3 +484,25 @@
         $scope.$apply();
     }, 700);
 }]);
+
+
+function changeDocumentFiles() {
+    var angularScope = angular.element('#DocumentationController').scope();
+
+    var files = $("#documentFilesInput")[0].files;
+
+    for (var i = 0; i < files.length; i++) {
+
+        let reader = new FileReader();
+        reader._index = i;
+        reader.onload = function (e) {
+            if (files[e.target._index].name, e.target.result.match(/,(.*)$/).length != 2)
+                return;
+
+            angularScope.Ctrls.DisplayDocs.admin.addNode.files.add(null, files[e.target._index].name, e.target.result.match(/,(.*)$/)[1], '.' + files[e.target._index].name.split('.')[1]);
+            angularScope.$apply();
+        };
+
+        reader.readAsDataURL(files[i]);
+    }
+};
